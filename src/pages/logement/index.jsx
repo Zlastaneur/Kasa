5ce415558ecd21294f42a6e9697964b3b237377f
Stanley <stanley.defer@hotmail.Fr>
2025-01-31 19:13:12 +0100

    ✨ Done "logement" page

--- src/pages/logement/index.jsx
+++ src/pages/logement/index.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import React from "react"
+
 import logements from "../../logements.json"
 import Carousel from "../../components/carousel/carousel"
 import Tags from "../../components/tags/tags"
 import Rating from "../../components/rating/rating"
 import styles from "./logement.module.scss"
-import React from "react"
+
+import { FaChevronUp } from "react-icons/fa6"
 
 function Logement() {
 	const { id } = useParams()
@@ -17,6 +20,15 @@ function Logement() {
 			setLogement(selectedLogement)
 		}
 	}, [id])
+
+	const [open, setOpen] = useState({})
+
+	const handleOpen = (id) => {
+		setOpen((previousState) => ({
+			...previousState,
+			[id]: !previousState[id],
+		}))
+	}
 
 	return (
 		<div className={styles.logement}>
@@ -42,6 +54,30 @@ function Logement() {
 						</div>
 					)}
 					<Rating data={logement.rating} />
+				</div>
+			</div>
+			<div className={styles.dropdown}>
+				<div className={styles.description}>
+					<button type="button" onClick={() => handleOpen(0)}>
+						Déscription
+						<FaChevronUp className={styles.arrow} />
+					</button>
+					{open[0] ? <p className={styles.open}>{logement.description}</p> : ""}
+				</div>
+				<div className={styles.equipements}>
+					<button type="button" onClick={() => handleOpen(1)}>
+						Équipements
+						<FaChevronUp className={styles.arrow} />
+					</button>
+					{open[1] ? (
+						<ul className={styles.open}>
+							{logement.equipments.map((equipement, index) => (
+								<li key={index}>{equipement}</li>
+							))}
+						</ul>
+					) : (
+						""
+					)}
 				</div>
 			</div>
 		</div>
